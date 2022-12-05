6982750b880fb2f668317e134c3bb94d9eca0850
Jayden Viegas <jaydenviegas57@gmail.com>
2022-12-05 20:08:08 +0530

    login/logout added

---
+++ Craftoza/Php/_register.php
@@ -0,0 +1,15 @@
+<div id="login">    
+    <?php include "C:/xampp/htdocs/DBProject/Craftoza/Php/_login.php";?>
+</div>
+<div id="signup">
+    <?php include "C:/xampp/htdocs/DBProject/Craftoza/Php/_signup.php";?>
+</div>
+<div id="fpwd">
+    <?php include "C:/xampp/htdocs/DBProject/Craftoza/Php/_forgotpassword.php";?>
+</div> 
+<div id="gotp">
+    <?php include "C:/xampp/htdocs/DBProject/Craftoza/Php/_getOTP.php";?>
+</div>
+<div id="npwd">
+    <?php include "C:/xampp/htdocs/DBProject/Craftoza/Php/_newpassword.php";?>
+</div>
