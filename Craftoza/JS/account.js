794bdbd6d7257e0d567c42eb46246baac5479299
Jayden Viegas <jaydenviegas57@gmail.com>
2022-12-11 13:38:37 +0530

    Account pages validation added updated

--- Craftoza/JS/account.js
+++ Craftoza/JS/account.js
@@ -22,8 +22,29 @@ function validateProfile(thisform){
     let mname=thisform.mname;
     let lname=thisform.lname;
     let mnum=thisform.mnum;
+    var test = /[0-9]/g;
 
+    if(!fname.value.match(test)) {  
+        fname.focus();
+        alert("Enter a Valid Name");
+        return false;
+    }
+    if(!mname.value.match(test)) {  
+        mname.focus();
+        alert("Enter a Valid Name");
+        return false;
+    }
+    if(!lname.value.match(test)) {  
+        lname.focus();
+        alert("Enter a Valid Name");
+        return false;
+    }
     if(mnum.value.length!=10){
+        thisform.mnum.focus();
+        alert("Enter a 10 Digit Number");
+        return false;
+    }
+    if(isNaN(mnum.value)){
         thisform.mnum.focus();
         alert("Enter a Valid Number");
         return false;
