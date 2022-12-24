065c53a830db5bdf2c5e8767473d79eeab73f572
Jayden Viegas <jaydenviegas57@gmail.com>
2022-12-24 17:43:15 +0530

    Minor updates

--- Craftoza/Php/_getOTP.php
+++ Craftoza/Php/_getOTP.php
@@ -7,23 +7,19 @@
     <p style="font-size: medium; color: rgb(44, 43, 43);">An OPT Has Been Sent To <?php if(isset($_SESSION['Email'])) {echo "{$_SESSION['Email']}";}else{echo "entered email";}?></p>
     
 
-    <!-- onsubmit="return validateOTP(this)" -->
     <form autocomplete="off" class="center2" id="OTPform">
         <br><br>
         <p id="resendotp">OTP has been resended</p>
         <p id='errmsg1'></p>
         <input type="number" id="getotp" name="otp"  class="inp otpinp" required>
         <br><br>
-        <!-- oninput="this.value=this.value.replace(/[^0-9]/g,'')" -->
 
 
-        <!-- <input type="submit" value="VERIFY" class="button2"><br><br> -->
         <button type="submit">VERIFY</button>
     </form>
 
     <form autocomplete="off" class="center2" id="ResendOTP">
         <button type="submit" id="rsend">RESEND OTP</button>
-        <!-- <p id="resend" style="font-size: 14px; padding-bottom: 10px;"></p> -->
     </form>
 
     <p style="font-size: 14px; padding-bottom: 40px;"><span onclick="displayNone('gotp');displayBlock('fpwd')">INVALID EMAIL?</span></p>
